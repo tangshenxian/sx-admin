38cf51df2be8f20e1aff7b71c7e3dba39a0fd5ab
tangshenxian <1125584876@qq.com>
2022-07-22 16:19:48 +0800

    mybatis-plus 自动填充

---
+++ sx-admin-common/src/main/java/com/shenxian/handler/MyMetaObjectHandler.java
@@ -0,0 +1,30 @@
+package com.shenxian.handler;
+
+import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.ibatis.reflection.MetaObject;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+
+/**
+ * 自动填充
+ * @author: shenxian
+ * @date: 2022/7/22 16:13
+ */
+@Slf4j
+@Component
+public class MyMetaObjectHandler implements MetaObjectHandler {
+    @Override
+    public void insertFill(MetaObject metaObject) {
+        log.info("start insert fill ......");
+        this.strictInsertFill(metaObject, "createTime", LocalDateTime.class, LocalDateTime.now());
+
+    }
+
+    @Override
+    public void updateFill(MetaObject metaObject) {
+        log.info("start update fill ......");
+        this.strictInsertFill(metaObject, "updateTime", LocalDateTime.class, LocalDateTime.now());
+    }
+}
